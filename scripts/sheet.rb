c5726537743acad816a0c20ee928ddc13aefa783
lilblizzard <lilblizzard97@gmail.com>
2021-10-01 16:59:39 -0400

    WIP: tif location

--- scripts/sheet.rb
+++ scripts/sheet.rb
@@ -5,15 +5,34 @@ NS = {
     mets: 'http://www.loc.gov/METS/',
 }
 
+institution = ARGV.first
+
 # loop through institution mets XMLs
-Dir["/Volumes/sceti-completed-4/DS-Legacy-Data/METS/digitalassets.lib.berkeley.edu/ds/#{ARGV[0]}/mets/*.xml"].each do |f|
+Dir["/Volumes/sceti-completed-4/DS-Legacy-Data/METS/digitalassets.lib.berkeley.edu/ds/#{institution}/mets/*.xml"].each do |f|
   xml = File.open(f) { |x| Nokogiri::XML x }
   nodes = xml.xpath('//mets:dmdSec[./mets:mdWrap/mets:xmlData/mods:mods/mods:identifier/@type="filename"]', NS)
   nodes.each do |node|
     dmdSec = node.attr("ID").to_s
-    mets_tif = node.xpath('//mets:dmdSec/mets:mdWrap/mets:xmlData/mods:mods/mods:identifier[@type="filename"]/text()')
+    mets_tif = node.xpath('mets:mdWrap/mets:xmlData/mods:mods/mods:identifier[@type="filename"]/text()')
+
+    case institution
+    when 'csl'
+    when 'cuny'
+    when 'conception'
+    when 'gts'
+    when 'grolier'
+    when 'indiana'
+    when 'nyu'
+    when 'providence'
+    when 'rutgers'
+    when 'nelsonatkins'
+    when 'ucb'
+    when 'kansas'
+    when 'wellesley'
+    end
+
     mets_tif.each do |m|
-      puts [ARGV.first, f, dmdSec, m.to_s].inspect
+      puts [institution, f.sub(%r{^/Volumes/sceti-completed-4/}, ''), dmdSec, m.to_s].inspect
     end
   end
 end
