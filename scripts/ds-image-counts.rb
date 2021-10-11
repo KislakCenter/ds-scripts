d62b8eceee0e45707f2874c5cefeb32fb2f48d6c
Doug Emery <emeryr@upenn.edu>
2021-10-11 12:18:49 -0500

    add header row; use DS10 constants

--- scripts/ds-image-counts.rb
+++ scripts/ds-image-counts.rb
@@ -4,43 +4,10 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
-# Institutions and their DS IDs
-#
-# conception    15
-# csl           13, 9
-# cuny           5
-# grolier       24
-# gts           23
-# indiana       40
-# kansas        30
-# nelsonatkins  46
-# nyu           25
-# providence    28
-# rutgers        6
-# ucb            1, 8, 11
-# wellesley     50
-
-inst_ids = {
-  15 => 'conception',
-  13 => 'csl',
-  9  => 'csl',
-  5  => 'cuny',
-  24 => 'grolier',
-  23 => 'gts',
-  40 => 'indiana',
-  30 => 'kansas',
-  46 => 'nelsonatkins',
-  25 => 'nyu',
-  28 => 'providence',
-  6  => 'rutgers',
-  1  => 'ucb',
-  8  => 'ucb',
-  11 => 'ucb',
-  50 => 'wellesley',
-}
-
 CSV.open('output.csv', 'w') do |csv|
-  inst_ids.each do |id, inst|
+  csv << %w{ inst callno count }
+  DS::INSTITUTION_DS_IDS.each do |id, inst|
+    institutions[inst] ||= {}
     uri = "https://digital-scriptorium.org/xtf3/search?rmode=digscript&smode=bid&bid=#{id}&docsPerPage=1000"
     ms_list = URI.open(uri) { |f| Nokogiri::HTML f }
     ms_list.xpath('//td/table[descendant::td/a[@class="headLink1"]]').each do |table|
