efb8766779d79a21d35780b2bb442e89036786de
Doug Emery <emeryr@upenn.edu>
2021-10-19 11:11:12 -0400

    Add institutions and alias values

--- lib/ds/constants.rb
+++ lib/ds/constants.rb
@@ -48,21 +48,42 @@ acknowledgements
 
     # TODO: Make source of institution Wikidata IDs configurable or dynamic
     INSTITUTION_QIDS_BY_NAME = {
+      'Beinecke Rare Book & Manuscript Library'   => 'https://www.wikidata.org/wiki/Q814779',
       'Bryn Mawr College'                         => 'https://www.wikidata.org/wiki/Q995265',
       'Chemical Heritage Foundation'              => 'https://www.wikidata.org/wiki/Q5090408',
       'City College of New York'                  => 'https://www.wikidata.org/wiki/Q1093910',
       'Columbia University'                       => 'https://www.wikidata.org/wiki/Q49088',
+      'Cornell University'                        => 'https://www.wikidata.org/wiki/Q49115',
       'Free Library of Philadelphia'              => 'https://www.wikidata.org/wiki/Q3087288',
       'General Theological Seminary'              => 'https://www.wikidata.org/wiki/Q1501676',
       'Grolier Club'                              => 'https://www.wikidata.org/wiki/Q5174002',
+      'Harvard University'                        => 'https://www.wikidata.org/wiki/Q13371',
+      'Huntington Library, Art Museum, and Botanical Gardens' => 'https://www.wikidata.org/wiki/Q1400558',
       'Nelson-Atkins Museum of Art'               => 'https://www.wikidata.org/wiki/Q1976985',
       'Philadelphia Museum of Art'                => 'https://www.wikidata.org/wiki/Q510324',
+      'Princeton University'                      => 'https://www.wikidata.org/wiki/Q21578',
       'Swarthmore College'                        => 'https://www.wikidata.org/wiki/Q1378320',
       'The College of Physicians of Philadelphia' => 'https://www.wikidata.org/wiki/Q5146808',
+      'The Huntington Library, Art Museum, and Botanical Gardens' => 'https://www.wikidata.org/wiki/Q1400558',
       'University of California, Berkeley'        => 'https://www.wikidata.org/wiki/Q168756',
       'University of Missouri'                    => 'https://www.wikidata.org/wiki/Q579968',
       'University of Pennsylvania'                => 'https://www.wikidata.org/wiki/Q49117',
       'Wellesley College'                         => 'https://www.wikidata.org/wiki/Q49205',
+      'Yale University'                           => 'https://www.wikidata.org/wiki/Q49112',
+    }
+
+    INSTITUTION_ALIASES = {
+      'beinecke'   => 'Beinecke Rare Book & Manuscript Library',
+      'columbia'   => 'Columbia University',
+      'cornell'    => 'Cornell University',
+      'flp'        => 'Free Library of Philadelphia',
+      'harvard'    => 'Harvard, University',
+      'huntington' => 'The Huntington Library, Art Museum, and Botanical Gardens',
+      'penn'       => 'University of Pennsylvania',
+      'princeton'  => 'Princeton University',
+      'upenn'      => 'University of Pennsylvania',
+      'wellesley'  => 'Wellesley College',
+      'yale'       => 'Yale University',
     }
 
     # Institutions dependent on DS and their DS IDs
