c4ed821907ddc5514d56b12946c7b99a0509ff17
Doug Emery <emeryr@upenn.edu>
2022-05-03 12:39:48 -0400

    Single method to clean up data

--- lib/ds/recon.rb
+++ lib/ds/recon.rb
@@ -1,6 +1,14 @@
 require_relative 'recon/names'
 require_relative 'recon/places'
 require_relative 'recon/subjects'
+require_relative 'recon/genre_terms'
 
 module Recon
+  def self.sort_and_dedupe array
+    if array.first.is_a? Array
+      array.sort { |a,b| a.first <=> b.first }.uniq &:join
+    else
+      array.sort.uniq
+    end
+  end
 end
