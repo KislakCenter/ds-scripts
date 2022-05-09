3f9ed4001b70f03b8531ef64c1e624626c5be85c
Doug Emery <emeryr@upenn.edu>
2022-05-09 17:13:05 -0400

    Script to run all the other collect scripts

---
+++ scripts/collect-all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+for x in "${SCRIPT_DIR}"/collect-prototype*.sh
+do
+  echo "=== running ${x} ==="
+  sh "${x}"
+done
+
+dstamp=$(date +%Y%m%d)
+dir=tmp/ds-data-${dstamp}
+mkdir ${dir}
+mkdir ${dir}/archive
+mv -v tmp/*-combined.csv ${dir}/
+mv -v tmp/*.csv ${dir}/archive
+
