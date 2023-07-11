c3e9ee4d7ad45dc7565246c958fcbe8a37b343ac
Doug Emery <emeryr@upenn.edu>
2023-07-11 16:49:06 -0400

    Use nokogiri to find marc by tag/code

--- scripts/find_marc_subfield.sh
+++ scripts/find_marc_subfield.sh
@@ -1,68 +1,83 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ruby
 
-# Usage: find_marc_code DATAFIELD_TAG SUBFIELD_CODE FILE[...]
-#
-# Prints out the field with code on context:
-#
-# Example: sh ./scripts/scratch_3.sh 245 f $(find data/prototype-data -type f -name \*.xml)
-#
-# data/prototype-data/cornell/cornell-marc-010.xml:  ><tag ind1='0' ind2='0' tag='245'
-# data/prototype-data/cornell/cornell-marc-010.xml-  ><code code='a'
-# data/prototype-data/cornell/cornell-marc-010.xml-    >Epistles of St. Paul, with commentary,</code
-# data/prototype-data/cornell/cornell-marc-010.xml-    ><code code='f'
-# data/prototype-data/cornell/cornell-marc-010.xml-    >[ca. 1100-1135]</code
-# --
-# data/prototype-data/cornell/cornell-marc-007.xml:  ><tag ind1='0' ind2='0' tag='245'
-# data/prototype-data/cornell/cornell-marc-007.xml-  ><code code='a'
-# data/prototype-data/cornell/cornell-marc-007.xml-    >Sermones,</code
-# data/prototype-data/cornell/cornell-marc-007.xml-    ><code code='f'
-# data/prototype-data/cornell/cornell-marc-007.xml-    >[13--]</code
-# ...
-#
+require 'nokogiri'
+require 'optparse'
 
-usage() {
-  echo "Usage: find_marc_code.sh DATAFIELD_TAG SUBFIELD_CODE FILE[...]"
-}
+CMD = File.basename __FILE__
 
-tag=$1
-shift
-code=$1
-shift
-files=$@
+def usage
+  "Usage: #{CMD} [OPTIONS] TAG [CODE...] FILE [FILE...]"
+end
 
-if ! egrep -q "^\d{3}$" <<< "${tag}"
-then
+options = { max_count: Float::INFINITY }
+OptionParser.new do |parser|
+  parser.banner = "#{usage}
+
+Print all MARC datafields in the provided files for the given datafield TAG
+and subfield CODE or CODEs. Subfield codes are optional.
+  "
+
+  parser.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  parser.on("-h", "--help", "Print this help") do
+    puts parser
+    exit
+  end
+
+  parser.on('-mCOUNT', '--max-files=COUNT', Integer,
+            'Maximum number of files with matching datafields to print') do |count|
+    options[:max_count] = count
+  end
+
+  parser.on('-f', '--files-only', 'Print only the names of matching files') do
+    options[:files_only] = true
+  end
+end.parse!
+
+##
+# INPUT
+##
+# the datafield tag
+tag = ARGV.shift
+unless tag =~ %r{^\d{3}$}
   usage
-  echo ""
-  echo "Not a 3-digit datafiled tag: '${tag}'"
-  exit 1
-fi
+  puts
+  abort "Tag should be a 3-digit number: '#{tag}'"
+end
 
-if ! egrep -q "^[a-z]$" <<< "${code}"
-then
-  usage
-  echo ""
-  echo "Not a subfield code: '${code}'"
-  exit 1
-fi
+# all single lower case letters and single digits are codes
+codes = []
+codes << ARGV.shift while ARGV.first =~ %r{^[a-z0-9]$}
 
-if [[ -z "$files" ]]
-then
-  usage
-  echo ""
-  echo "No files provided"
-  exit 1
-fi
+# assume the rest are files
+files = ARGV
 
-egrep -A 4 "tag=.${tag}." ${files} | egrep "code=.${code}." | \
-while read line
-do
- file=$(awk '{ print $1}' | sed 's/-$//')
- egrep -A 6 "tag=.${tag}." ${file}
-done
+# buuld xpath query for the any codes given; otherwise, use empty string ''
+if codes.empty?
+  subfields = ''
+else
+  subfields = " and ./subfield[#{codes.map { |c|
+    "@code='#{c}'"
+  }.join(' or ')}]"
+end
+xpath = "record/datafield[@tag=#{tag}#{subfields}]"
+puts "Using xpath: #{xpath}" if options[:verbose]
 
-if [[ $? -ne 0 ]]
-then
-  usage
-  exit 1
-fi
+# set a counter in case there's a limit
+count = 0
+files.each do |file|
+  xml = File.open(file) { |f| Nokogiri::XML(f) }
+  xml.remove_namespaces!
+  xml.xpath(xpath).each_with_index do |node, ndx|
+    count += 1 if ndx == 0
+    if options[:files_only]
+      puts file
+    else
+      puts "============ #{file} ==========="
+      puts node.to_xml
+    end
+  end
+  break if count >= options[:max_count] # not tested whether this is needed
+end
