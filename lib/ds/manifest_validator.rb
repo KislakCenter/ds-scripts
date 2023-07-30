59d9af1dedbc5aa7790982d6f583a278996c9565
Doug Emery <emeryr@upenn.edu>
2023-07-30 10:53:54 -0400

    Move column constants to DS::Manifest

--- lib/ds/manifest_validator.rb
+++ lib/ds/manifest_validator.rb
@@ -23,89 +23,6 @@ module DS
     attr_reader :manifest
     attr_reader :source_dir
 
-    def self.normalize_lookup value
-      return '' if value.blank?
-      value.to_s.downcase.strip.gsub %r{\W+}, ''
-    end
-
-    INSTITUTION_WIKIDATA_QID            = 'holding_institution_wikidata_qid'
-    FILENAME                            = 'filename'
-    INSTITUTION_WIKIDATA_LABEL          = 'holding_institution_wikidata_label'
-    SOURCE_TYPE                         = 'source_data_type'
-    DS_ID                               = 'ds_id'
-    INSTITUTIONAL_ID                    = 'holding_institution_institutional_id'
-    INSTITUTIONAL_ID_LOCATION_IN_SOURCE = 'institutional_id_location_in_source'
-    RECORD_LAST_UPDATED                 = 'record_last_updated'
-    CALL_NUMBER                         = 'call_number'
-    TITLE                               = 'title'
-    IIIF_MANIFEST_URL                   = 'iiif_manifest_url'
-    LINK_TO_INSTITUTIONAL_RECORD        = 'link_to_institutional_record'
-    MANIFEST_GENERATED_AT               = 'manifest_generated_at'
-
-    MANIFEST_COLUMNS = [
-      INSTITUTION_WIKIDATA_QID,
-      INSTITUTION_WIKIDATA_LABEL,
-      FILENAME,
-      SOURCE_TYPE,
-      DS_ID,
-      INSTITUTIONAL_ID,
-      INSTITUTIONAL_ID_LOCATION_IN_SOURCE,
-      RECORD_LAST_UPDATED,
-      CALL_NUMBER,
-      TITLE,
-      IIIF_MANIFEST_URL,
-      LINK_TO_INSTITUTIONAL_RECORD,
-      MANIFEST_GENERATED_AT
-    ].freeze
-
-    REQUIRED_VALUES = [
-      INSTITUTION_WIKIDATA_QID,
-      FILENAME,
-      INSTITUTION_WIKIDATA_LABEL,
-      SOURCE_TYPE,
-      INSTITUTIONAL_ID,
-      INSTITUTIONAL_ID_LOCATION_IN_SOURCE,
-      RECORD_LAST_UPDATED,
-      CALL_NUMBER,
-      MANIFEST_GENERATED_AT
-    ].freeze
-
-    URI_COLUMNS = [
-      LINK_TO_INSTITUTIONAL_RECORD,
-      IIIF_MANIFEST_URL
-    ].freeze
-
-    QID_COLUMNS = [
-      INSTITUTION_WIKIDATA_QID
-    ].freeze
-
-    DATE_TIME_COLUMNS = [
-      RECORD_LAST_UPDATED,
-      MANIFEST_GENERATED_AT
-    ].freeze
-
-    MARC_XML = 'MARC XML'
-    TEI_XML  = 'TEI XML'
-    DS_CSV   = 'DS CSV'
-    DS_METS  = 'DS METS XML'
-
-    # source type list of all type names and normalized names; i.e.,
-    # lower case names stripped of all whitespace and non-word characters
-    VALID_SOURCE_TYPES = [
-      MARC_XML,
-      TEI_XML,
-      DS_CSV,
-      DS_METS
-    ].freeze
-
-    SOURCE_TYPE_LOOKUP = VALID_SOURCE_TYPES.flat_map { |type|
-      normal = ManifestValidator.normalize_lookup type
-      [type, normal]
-    }.freeze
-
-    XML_SOURCE_TYPES = SOURCE_TYPE_LOOKUP.select { |t| t =~ %r{xml}i }
-    CSV_SOURCE_TYPES = SOURCE_TYPE_LOOKUP.select { |t| t =~ %r{csv}i }
-
     URI_REGEXP = URI::DEFAULT_PARSER.make_regexp %w{http https}
     QID_REGEXP = %r{\AQ\d+\z}
 
@@ -131,7 +48,7 @@ module DS
     # @return [boolean] true if all required columns are present
     def validate_columns
       found_columns = manifest.csv.first.headers
-      diff = MANIFEST_COLUMNS - found_columns
+      diff = Manifest::MANIFEST_COLUMNS - found_columns
       return true if diff.empty?
       STDERR.puts "Manifest missing required columns: #{diff.join ', '}"
     end
@@ -141,7 +58,7 @@ module DS
     def validate_required_values
       is_valid = true
       manifest.csv.each do |row|
-        REQUIRED_VALUES.each_with_index do |col, ndx|
+        Manifest::REQUIRED_VALUES.each_with_index do |col, ndx|
           if row[col].blank?
             STDERR.puts "Required value missing row: #{ndx}; col.: #{col}"
             is_valid = false
@@ -168,10 +85,10 @@ module DS
     def validate_files_exist
       is_valid = true
       manifest.csv.each_with_index do |row, row_num|
-        file_path = File.join manifest.source_dir, row[FILENAME]
+        file_path = File.join manifest.source_dir, row[Manifest::FILENAME]
         unless File.exist? file_path
           is_valid = false
-          STDERR.puts "Source file not found row: #{row_num}; source directory: #{source_dir}; file: #{row[FILENAME]}"
+          STDERR.puts "Source file not found row: #{row_num}; source directory: #{source_dir}; file: #{row[Manifest::FILENAME]}"
         end
       end
       is_valid
@@ -183,11 +100,11 @@ module DS
     def validate_ids
       is_valid = true
       manifest.csv.each_with_index do |row, row_num|
-        file_path      = File.join manifest.source_dir, row[FILENAME]
-        source_type    = row[SOURCE_TYPE]
+        file_path      = File.join manifest.source_dir, row[Manifest::FILENAME]
+        source_type    = row[Manifest::SOURCE_TYPE]
 
-        normal_source  = ManifestValidator.normalize_lookup source_type
-        inst_id        = row[INSTITUTIONAL_ID]
+        normal_source  = Manifest.normalize_lookup source_type
+        inst_id        = row[Manifest::INSTITUTIONAL_ID]
         found = case normal_source
                 when 'MARC XML', 'marcxml'
                   id_in_marc_xml? file_path, inst_id
@@ -195,7 +112,7 @@ module DS
                   raise NotImplementedError("validate_ids not implemented for: #{source_type}")
                 end
         unless found
-          STDERR.puts "ID not found in source file row: #{row}; id: #{inst_id}; source_file: #{row[FILENAME]}"
+          STDERR.puts "ID not found in source file row: #{row}; id: #{inst_id}; source_file: #{row[Manifest::FILENAME]}"
           is_valid = false
         end
       end
@@ -228,10 +145,10 @@ module DS
     ####################################
     def validate_source_type row, row_num
       is_valid = true
-      col = SOURCE_TYPE
+      col = Manifest::SOURCE_TYPE
 
-      unless SOURCE_TYPE_LOOKUP.include? ManifestValidator.normalize_lookup row[col]
-        STDERR.puts "Invalid source type in row: #{row_num}; expected one of #{VALID_SOURCE_TYPES.join ', '}; got: '#{row[col]}'"
+      unless Manifest::SOURCE_TYPE_LOOKUP.include? Manifest.normalize_lookup row[col]
+        STDERR.puts "Invalid source type in row: #{row_num}; expected one of #{Manifest::VALID_SOURCE_TYPES.join ', '}; got: '#{row[col]}'"
         is_valid = false
       end
       is_valid
@@ -239,7 +156,7 @@ module DS
 
     def validate_urls row, row_num
       is_valid = true
-      URI_COLUMNS.each do |col|
+      Manifest::URI_COLUMNS.each do |col|
         unless row[col].to_s =~ URI_REGEXP
           STDERR.puts "Invalid URL in row: #{row_num}; col.: #{col}: '#{row[col]}'"
           is_valid = false
@@ -250,7 +167,7 @@ module DS
 
     def validate_qids row, row_num
       is_valid = true
-      QID_COLUMNS.each do |col|
+      Manifest::QID_COLUMNS.each do |col|
         unless row[col].to_s =~ QID_REGEXP
           is_valid = false
           STDERR.puts "Invalid QID in row: #{row_num}; col.: #{col}: '#{row[col]}'"
@@ -261,7 +178,7 @@ module DS
 
     def validate_dates row, row_num
       is_valid = true
-      DATE_TIME_COLUMNS.each do |col|
+      Manifest::DATE_TIME_COLUMNS.each do |col|
         next if row[col].blank?
         begin
           Date.parse row[col]
