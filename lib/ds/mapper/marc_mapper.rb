5db8e859a62d4944f1cb69a1687b7b6f4aba09fe
Doug Emery <emeryr@upenn.edu>
2023-11-21 22:20:10 -0500

    Remove superfluous attribute readers

--- lib/ds/mapper/marc_mapper.rb
+++ lib/ds/mapper/marc_mapper.rb
@@ -5,17 +5,7 @@ module DS
     class MarcMapper
       attr_reader :manifest_entry
       attr_reader :record
-      attr_reader :inst_qid
-      attr_reader :inst_code
-      attr_reader :preferred_name
-      attr_reader :holdings_file
       attr_reader :timestamp
-      attr_reader :source_file
-      attr_reader :source_file
-      attr_reader :iiif_manifest_url
-      attr_reader :institutional_id
-      attr_reader :call_number
-      attr_reader :link_to_institutional_record
 
       ##
       # @param [DS::Manifest:Entry] manifest_entry the manifest line
@@ -25,25 +15,19 @@ module DS
       def initialize(manifest_entry:, record:, timestamp:)
         @record                       = record
         @manifest_entry               = manifest_entry
-        @inst_qid                     = manifest_entry.institution_wikidata_qid
-        @preferred_name               = manifest_entry.institution_wikidata_label
         @timestamp                    = timestamp
-        @source_file                  = manifest_entry.filename
-        @iiif_manifest_url            = manifest_entry.iiif_manifest_url
-        @institutional_id             = manifest_entry.institutional_id
-        @call_number                  = manifest_entry.call_number
-        @link_to_institutional_record = manifest_entry.link_to_institutional_record
       end
 
       def map_record
         source_type                        = 'marc-xml'
+        source_file                        = manifest_entry.filename
         cataloging_convention              = DS::MarcXML.extract_cataloging_convention record
-        holding_institution                = inst_qid
-        holding_institution_as_recorded    = preferred_name
+        holding_institution                = manifest_entry.institution_wikidata_qid
+        holding_institution_as_recorded    = manifest_entry.institution_wikidata_label
         holding_institution_id_number      = DS::MarcXML.extract_001_control_number record
-        holding_institution_shelfmark      = call_number
-        link_to_holding_institution_record = link_to_institutional_record
-        iiif_manifest                      = iiif_manifest_url
+        holding_institution_shelfmark      = manifest_entry.call_number
+        link_to_holding_institution_record = manifest_entry.link_to_institutional_record
+        iiif_manifest                      = manifest_entry.iiif_manifest_url
         production_date_encoded_008        = DS::MarcXML.extract_encoded_date_008 record
         production_date                    = DS::MarcXML.parse_008 production_date_encoded_008, range_sep: '^'
         century                            = DS.transform_dates_to_centuries production_date
