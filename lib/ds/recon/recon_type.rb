8f1ab19d579b5e4fad248429bf5dd1ac5a878f82
Doug Emery <emeryr@upenn.edu>
2024-05-21 13:46:22 -0400

    Remove look up columns from recon settings

--- lib/ds/recon/recon_type.rb
+++ lib/ds/recon/recon_type.rb
@@ -36,6 +36,21 @@ module Recon
         self::METHOD_NAME
       end
 
+      # Returns the balanced columns for the current object.
+      #
+      # Balanced columns should have equal numbers of fields and
+      # subfields in each row; e.g., if fields are delimited by '|'
+      # and subfields by ';', then the following are balanced:
+      #
+      # structured_value,authorized_label
+      # a|b;c,d|e;f
+      # 1|2|3,x|y|z
+      # r,s
+      #
+      # @return [Array<Symbol>] The balanced columns.
+      #
+      # @example
+      #   Recon::Materials.balanced_columns #=> [:structured_value, :authorized_label]
       def balanced_columns
         self::BALANCED_COLUMNS
       end
