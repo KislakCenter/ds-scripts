2812be3c111704a0c84209bc25900c939ae67cdf
Doug Emery <emeryr@upenn.edu>
2024-06-05 09:35:07 -0400

    Remove unused/commented code

--- lib/ds/manifest/simple_xml_id_validator.rb
+++ lib/ds/manifest/simple_xml_id_validator.rb
@@ -29,28 +29,14 @@ module DS
         locator = DS::Extractor::XmlRecordLocator.new namespaces: namespaces
         source = find_or_open_source source_path
         locator.locate_record source, id, id_location
-        # xml = find_or_open_source source_path
-        # formatted_location = id_location.gsub('ID_PLACEHOLDER', id)
-        #
-        # # try with namespaces
-        # record = try_locate_record xml, formatted_location, namespaces: namespaces
-        # return record if record.present?
-        #
-        # # try without providing namespaces
-        # record = try_locate_record xml, formatted_location
-        # return record if record.present?
-        #
-        # # strip namespaces and try one last time
-        # xml.remove_namespaces!
-        # try_locate_record xml, formatted_location
       end
 
-      def try_locate_record xml, xpath, namespaces: nil
-        xml.xpath xpath, namespaces
-      rescue Nokogiri::XML::XPath::SyntaxError => e
-        raise unless e.message =~ /undefined namespace prefix/i
-        []
-      end
+      # def try_locate_record xml, xpath, namespaces: nil
+      #   xml.xpath xpath, namespaces
+      # rescue Nokogiri::XML::XPath::SyntaxError => e
+      #   raise unless e.message =~ /undefined namespace prefix/i
+      #   []
+      # end
 
       # Opens the XML source file at the given path and returns an XML document without namespaces.
       #
